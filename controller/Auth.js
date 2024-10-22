9f0264bb9f230f77d77019ba4f0976e2db403322
anamul <anamul.haque@adndiginet.com>
2024-10-22 17:11:38 +0600

    login issue is solved

--- controller/Auth.js
+++ controller/Auth.js
@@ -51,7 +51,7 @@ export async function loginUser(req, res) {
       httpOnly: true,
     })
     .status(201)
-    .json({ id: user.id, role: user.role, name: doc.name });
+    .json({ id: user.id, role: user.role, name: user?.name });
 }
 
 export async function logout(req, res) {
