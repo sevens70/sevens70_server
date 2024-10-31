e42930c6c7ce5afa8b104e6a9a160494ad90e73b
anamul <anamul.haque@adndiginet.com>
2024-10-31 11:22:41 +0600

    verifying token with headers also

--- services/common.js
+++ services/common.js
@@ -1,28 +1,35 @@
 import passport from "passport";
 
 export const isAuth = () => {
-  return (req, res, next) => {
-    passport.authenticate("jwt", { session: false }, (err, user, info) => {
-      if (err) {
-        return res.status(500).json({ message: "Internal Server Error" });
-      }
-      if (!user) {
-        return res.status(401).json({ message: "Unauthorized" });
-      }
-      req.user = user; // Attach the authenticated user to the request
-      next(); // Proceed to the next middleware or route handler
-    })(req, res, next);
-  };
+  return passport.authenticate("jwt");
 };
 
 export const sanitizeUser = (user) => {
   return { id: user.id, role: user.role };
 };
 
+// export const cookieExtractor = function (req) {
+//   let token = null;
+//   if (req && req.cookies) {
+//     token = req.cookies["jwt"];
+//   }
+//   return token;
+// };
 export const cookieExtractor = function (req) {
   let token = null;
-  if (req && req.cookies) {
+
+  // Check for token in Authorization header
+  if (req.headers && req.headers.authorization) {
+    const authHeader = req.headers.authorization;
+    if (authHeader.startsWith("Bearer ")) {
+      token = authHeader.slice(7, authHeader.length); // Remove "Bearer " prefix
+    }
+  }
+
+  // If token not found in Authorization header, check cookies
+  if (!token && req.cookies) {
     token = req.cookies["jwt"];
   }
+
   return token;
 };
