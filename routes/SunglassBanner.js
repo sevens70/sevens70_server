91f1185410851b4d458f50df99b3c39f42c4cef2
anamul01988 <anamulhaque0198877@gmail.com>
2024-12-21 19:03:25 +0600

    Clean cache and dependencies

--- routes/SunglassBanner.js
+++ routes/SunglassBanner.js
@@ -1,22 +1,22 @@
-import express from "express";
+// import express from "express";
+// // import {
+// //   createBrand,
+// //   deleteBrand,
+// //   fetchBrands,
+// //   updateBrand,
+// // } from "../controller/Brand.js";
 // import {
-//   createBrand,
-//   deleteBrand,
-//   fetchBrands,
-//   updateBrand,
-// } from "../controller/Brand.js";
-import {
-  createSunglassBanner,
-  deleteSunglassBanner,
-  fetchSunglassBanner,
-  updateSunglassBanner,
-} from "../controller/SunglassBanner.js";
+//   createSunglassBanner,
+//   deleteSunglassBanner,
+//   fetchSunglassBanner,
+//   updateSunglassBanner,
+// } from "../controller/SunglassBanner.js";
 
-const sunglassBannerRouter = express.Router();
-sunglassBannerRouter
-  .get("/", fetchSunglassBanner)
-  .post("/", createSunglassBanner)
-  .patch("/:id", updateSunglassBanner)
-  .delete("/:id", deleteSunglassBanner);
+// const sunglassBannerRouter = express.Router();
+// sunglassBannerRouter
+//   .get("/", fetchSunglassBanner)
+//   .post("/", createSunglassBanner)
+//   .patch("/:id", updateSunglassBanner)
+//   .delete("/:id", deleteSunglassBanner);
 
-export { sunglassBannerRouter };
+// export { sunglassBannerRouter };
--- routes/SunglassProduct.js
+++ routes/SunglassProduct.js
@@ -1,22 +1,22 @@
-import express from "express";
+// import express from "express";
+// // import {
+// //   createBrand,
+// //   deleteBrand,
+// //   fetchBrands,
+// //   updateBrand,
+// // } from "../controller/Brand.js";
 // import {
-//   createBrand,
-//   deleteBrand,
-//   fetchBrands,
-//   updateBrand,
-// } from "../controller/Brand.js";
-import {
-  createsunglassProduct,
-  deletesunglassProduct,
-  fetchsunglassProduct,
-  updatesunglassProduct,
-} from "../controller/sunglassProduct.js";
+//   createsunglassProduct,
+//   deletesunglassProduct,
+//   fetchsunglassProduct,
+//   updatesunglassProduct,
+// } from "../controller/sunglassProduct.js";
 
-const sunglassProductRouter = express.Router();
-sunglassProductRouter
-  .get("/", fetchsunglassProduct)
-  .post("/", createsunglassProduct)
-  .patch("/:id", updatesunglassProduct)
-  .delete("/:id", deletesunglassProduct);
+// const sunglassProductRouter = express.Router();
+// sunglassProductRouter
+//   .get("/", fetchsunglassProduct)
+//   .post("/", createsunglassProduct)
+//   .patch("/:id", updatesunglassProduct)
+//   .delete("/:id", deletesunglassProduct);
 
-export { sunglassProductRouter };
+// export { sunglassProductRouter };
